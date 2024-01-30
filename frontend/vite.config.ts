aa4dc853f73cb03e7f41a5e66856d29d147c66df
shahir abdulla <shahirmc5565@gmail.com>
2024-01-30 21:52:43 +0530

    docker setup done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // we need to define which port we will run on
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  }
+  
 })
